6748f9fb6e2de1b31b96adcb75b1f065a0d28afa
tehsin118 <octaloop.tech@gmail.com>
2025-10-10 17:09:48 +0500

    Refactor HomeFooter component styles for improved layout consistency

--- src/app/(main-page)/HomeFooter.tsx
+++ src/app/(main-page)/HomeFooter.tsx
@@ -4,18 +4,18 @@ import React from "react";
 
 const HomeFooter = ({ dict }: { dict: Dictionary }) => {
   return (
-    <div className="bg-[#181818] pt-[42px]">
+    <div className="bg-[#181818] py-10">
       <div className="home-wrapper">
-        <footer className="flex-col-center gap-[30px] text-center">
+        <footer className="flex-col-center gap-[30px] text-center  ">
           <Logo titleClassName="text-white" />
 
           <p className="text-[14px] font-[300] leading-[2] max-w-[60%] text-white/80 max-sm:max-w-[80%] max-xs:text-[12px] max-xs:max-w-full">
             {dict?.home?.footer?.title}
           </p>
-
+          {/* 
           <p className="w-full py-[15px] text-[14px] opacity-[0.75] font-[500] text-white border-t border-gray-b/20">
             © {new Date().getFullYear()} | octaloop.io {dict?.home?.footer?.copyright}
-          </p>
+          </p> */}
         </footer>
       </div>
     </div>
