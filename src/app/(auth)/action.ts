d795fa374e84da1c328d78d92f3572128c60e2db
mudassar <mudassarr640@gmail.com>
2025-10-22 15:29:29 +0500

    added query for zoho login

--- src/app/(auth)/action.ts
+++ src/app/(auth)/action.ts
@@ -18,7 +18,7 @@ export const loginWithGoogle = async () => {
 // ==============================================================
 export const loginWithZoho = async () => {
   // Redirect to backend Zoho CRM OAuth endpoint
-  const zohoAuthUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/companies/auth/zoho`;
+  const zohoAuthUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/companies/auth/zoho?type=login`;
   redirect(zohoAuthUrl);
 };
 
